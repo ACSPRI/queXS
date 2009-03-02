7e4ade1ade47fc79a2e4d9f11f10a8c693817618
azammitdcarf <azammitdcarf>
2009-03-02 06:42:19 +0000

    Bug in samplesearch.php where multiple records are returned for a sample record. The number of records returned is the same as the number of sample_vars

--- admin/samplesearch.php
+++ admin/samplesearch.php
@@ -101,7 +101,8 @@ if ($sample_import_id != false)
 			FROM sample_var AS sv
 			JOIN (sample as s) ON (s.import_id = '$sample_import_id' and sv.sample_id = s.sample_id)
 			LEFT JOIN (`case` AS c, questionnaire AS q) ON ( c.sample_id = sv.sample_id AND q.questionnaire_id = c.questionnaire_id )
-			WHERE sv.val LIKE $search";
+			WHERE sv.val LIKE $search
+			GROUP BY s.sample_id";
 
 		$r = $db->GetAll($sql);
 
