4c5768bc2fe0b1ff1a8ddbe256ae4f9a268b57ce
azammitdcarf <azammitdcarf>
2009-03-17 06:01:37 +0000

    Added databasestrings

---
+++ admin/databasestrings.php
@@ -0,0 +1,70 @@
+<?
+/**
+ * A list of database strings for translation
+ *
+ *
+ *	This file is part of queXS
+ *	
+ *	queXS is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	queXS is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with queXS; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * @author Adam Zammit <adam.zammit@deakin.edu.au>
+ * @copyright Deakin University 2007,2008
+ * @package queXS
+ * @subpackage admin
+ * @link http://www.deakin.edu.au/dcarf/ queXS was writen for DCARF - Deakin Computer Assisted Research Facility
+ * @license http://opensource.org/licenses/gpl-2.0.php The GNU General Public License (GPL) Version 2
+ *
+ */
+
+T_("Not attempted or worked");
+T_("No answer");
+T_("Technical phone problems");
+T_("Other, Referred to Supervisor (Eligible)");
+T_("Other, Referred to Supervisor (Unknown eligibility)");
+T_("Soft Refusal, Other");
+T_("Hard Refusal, Other");
+T_("Soft Refusal, Respondent");
+T_("Hard Refusal, Respondent");
+T_("Complete");
+T_("Known respondent refusal");
+T_("Household-level refusal");
+T_("Broken appointment (Implicit refusal)");
+T_("Disconnected number");
+T_("Fax/data line");
+T_("Business, government office, other organization");
+T_("No eligible respondent");
+T_("Accidental hang up or temporary phone problem");
+T_("Definite Appointment - Respondent");
+T_("Definite Appointment - Other");
+T_("Unspecified Appointment - Respondent");
+T_("Unspecified Appointment - Other");
+T_("Household answering machine - Message left");
+T_("Household answering machine - No message left");
+T_("Respondent Dead");
+T_("Physically or mentally unable/incompetent");
+T_("Household level language problem");
+T_("Respondent language problem");
+T_("Answering machine - Not a household");
+T_("Out of sample");
+T_("Non contact");
+T_("Temporary Outcomes (normal cases)");
+T_("Supervisor Outcomes (referred to supervisor)");
+T_("Refusal Outcomes (respondent refused)");
+T_("Final Outcomes (completed, final refusal, etc)");
+T_("Appointments");
+
+
+?>
