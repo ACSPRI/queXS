0ce5b5ef673a47a8d993dcc745aa7a41e2ab2b7c
Adam Zammit <adam.zammit@acspri.org.au>
2013-06-18 16:06:27 +1000

    Define availability group as for respondents

--- admin/availabilitygroup.php
+++ admin/availabilitygroup.php
@@ -69,6 +69,7 @@ global $db;
 
 xhtml_head(T_("Availability groups"),true,array("../css/table.css"),array("../js/window.js"));
 
+
 if (isset($_GET['availability_group']))
 {
 	$availability_group = $db->qstr($_GET['availability_group']);
@@ -87,6 +88,8 @@ $sql = "SELECT description,
 	$rs = $db->GetAll($sql);
 
 print "<h3>" . T_("Availability groups")."</h3>";
+
+print "<p>" . T_("Availability groups define periods of time of respondent availability.") . "</p>";
 
 if (empty($rs))
 	print "<p>" . T_("No availability groups") . "</p>";
