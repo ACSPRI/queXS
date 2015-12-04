7135118350971fe614e1387dbe61a3004b66e058
Adam Zammit <adam.zammit@acspri.org.au>
2015-12-04 14:40:35 +1100

    Fixed error with wildcards

--- locale/updatemo.sh
+++ locale/updatemo.sh
@@ -2,5 +2,7 @@
 for f in `find ./ -iname *.po`; do
    BASE=`basename $f .po`
    DIR=`dirname $f`
+   grep -v '#, php-format' $f > $DIR/tmpfile
+   mv $DIR/tmpfile $f
    msgfmt $f -o $DIR/$BASE.mo
 done
