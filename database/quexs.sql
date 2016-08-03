5e71e9d109fe8173a6a9467cde6498c864bab376
Adam Zammit <adam.zammit@acspri.org.au>
2016-08-03 11:02:44 +1000

    Database and function changes for weighted timeslots

--- database/quexs.sql
+++ database/quexs.sql
@@ -1353,6 +1353,7 @@ CREATE TABLE `questionnaire_sample_timeslot` (
   `questionnaire_id` bigint(20) NOT NULL,
   `sample_import_id` bigint(20) NOT NULL,
   `availability_group_id` bigint(20) NOT NULL,
+  `weight` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`questionnaire_id`,`availability_group_id`,`sample_import_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -1365,6 +1366,7 @@ CREATE TABLE `questionnaire_sample_timeslot` (
 CREATE TABLE `questionnaire_timeslot` (
   `questionnaire_id` bigint(20) NOT NULL,
   `availability_group_id` bigint(20) NOT NULL,
+  `weight` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`questionnaire_id`,`availability_group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
