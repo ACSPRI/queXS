823cb4bc69ace94ee0038d6b86456a2d1e90f0a8
Adam Zammit <adam.zammit@acspri.org.au>
2016-11-21 13:03:31 +1100

    Added port configuration

--- config.default.php
+++ config.default.php
@@ -134,6 +134,12 @@ if (!defined('LIME_AUTO_ADVANCE')) define('LIME_AUTO_ADVANCE', true);
  */
 if (!defined('QUEXS_PATH')) define('QUEXS_PATH', '/quexs/');
 
+/**
+ * The port queXS is running on (default blank, use :8080 for example if using 
+ * port 8080)
+ */
+if (!defined('QUEXS_PORT')) define('QUEXS_PORT', '');
+
 $protocol = "http://";
 
 if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) 
@@ -144,12 +150,12 @@ if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER
 /**
  * The complete URL to limesurvey
  */
-if (!defined('LIME_URL')) define('LIME_URL', $protocol . $_SERVER['SERVER_NAME'] . QUEXS_PATH . LIME_PATH);
+if (!defined('LIME_URL')) define('LIME_URL', $protocol . $_SERVER['SERVER_NAME'] . QUEXS_PORT . QUEXS_PATH . LIME_PATH);
 
 /**
  * The complete URL to this copy of queXS
  */
-if (!defined('QUEXS_URL')) define('QUEXS_URL', $protocol . $_SERVER['SERVER_NAME'] . QUEXS_PATH);
+if (!defined('QUEXS_URL')) define('QUEXS_URL', $protocol . $_SERVER['SERVER_NAME'] . QUEXS_PORT . QUEXS_PATH);
 
 /**
  * The default locale (language)
